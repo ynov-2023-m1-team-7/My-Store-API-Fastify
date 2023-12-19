74b4b669f3748831e4db67bd3b4ac6ae798cdca7
Damien Drozd <damien.drozd@icloud.com>
2023-12-19 13:10:24 +0100

    moogoose test

--- src/app.js
+++ src/app.js
@@ -1,18 +1,34 @@
-const fastify = require("fastify");
+const fastify = require("fastify")({ logger: true });
 const cors = require("@fastify/cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 fastify.register(cors, {
   origin: "*",
 });
 
-fastify.register(require("@fastify/mongodb"), {
-  url: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PWD}@${process.env.MONGODB_URL}/?retryWrites=true&w=majority`,
-});
+mongoose.set("strictQuery", false);
+//Refaire la connection avec mongoose
+mongoose.connect(
+  `mongodb://${process.env.MONGODB_URL}/${process.env.MONGODB_NAME}`
+).then(() => {
+  console.log("successfully connect to database")
+}).catch(err => console.log(err))
 
 // Declare a route
 fastify.get("/", function (request, reply) {
   reply.send({ hello: "world" });
 });
+
+
+//refaire la route mais avec mongoose
+// fastify.get("/getProducts", async function (request, reply) {
+//   try {
+//     const result = await Product.find({});
+//     reply.send(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 // Run the server!
 fastify.listen(
