805cdebc9ada9b0c0365eeb2e799d76801fa31bf
Damien Drozd <damien.drozd@icloud.com>
2023-12-19 15:21:25 +0100

    feature(backend):redis on fastify and recomandation route

--- src/app.js
+++ src/app.js
@@ -1,42 +1,81 @@
 const fastify = require("fastify")({ logger: true });
 const cors = require("@fastify/cors");
 const mongoose = require("mongoose");
+const fastifyRedis = require('@fastify/redis')
+
+
 require("dotenv").config();
 fastify.register(cors, {
   origin: "*",
 });
 
 mongoose.set("strictQuery", false);
-//Refaire la connection avec mongoose
+// //Refaire la connection avec mongoose
 mongoose.connect(
   `mongodb://${process.env.MONGODB_URL}/${process.env.MONGODB_NAME}`
 ).then(() => {
-  console.log("successfully connect to database")
+  console.log("successfully connect to database mongodb")
 }).catch(err => console.log(err))
+
+fastify.register(require('@fastify/redis'), {
+  host: '127.0.0.1',
+  password: 'root',
+  port: 6379, // Redis port
+})
+
 
 // Declare a route
 fastify.get("/", function (request, reply) {
   reply.send({ hello: "world" });
 });
 
+// route pour récupérer les produits avec redis
+fastify.get("/getProducts", async function (request, reply) {
+  try {
+    //recupérer la fouchette de prix dans les paramètres de la requête
+    const { min, max } = request.query;
 
-//refaire la route mais avec mongoose
-// fastify.get("/getProducts", async function (request, reply) {
-//   try {
-//     const result = await Product.find({});
-//     reply.send(result);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
+    const randomProducts = [];
+    const result = await fastify.redis.get("products");
+    const products = JSON.parse(result);
+    const filteredProducts = products.filter(
+      (product) => product.price >= min && product.price <= max
+    );
+    // si il n'y a pas assez de produits dans la fourchette de prix, on renvoit 3 produits aléatoires
+    while (filteredProducts.length < 3) {
+      const randomIndex = Math.floor(Math.random() * products.length);
+      // on vérifie que le produit n'est pas déjà dans le tableau
+      if (filteredProducts.includes(products[randomIndex])) {
+        continue;
+      }
+      filteredProducts.push(products[randomIndex]);
+    }
+    if (filteredProducts.length === 3) {
+      reply.send(filteredProducts);
+    } else {
+      while (randomProducts.length < 3) {
+        const randomIndex = Math.floor(Math.random() * filteredProducts.length);
+        // on vérifie que le produit n'est pas déjà dans le tableau
+        if (randomProducts.includes(filteredProducts[randomIndex])) {
+          continue;
+        }
+        randomProducts.push(filteredProducts[randomIndex]);
+      }
+      reply.send(randomProducts);
+    }
+
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 
 // Run the server!
-fastify.listen(
-  { port: process.env.PORT, host: "127.0.0.1" },
-  function (err, address) {
-    if (err) {
-      fastify.log.error(err);
-      process.exit(1);
-    }
+fastify.listen({ port: process.env.PORT }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
   }
-);
+  // Server is now listening on ${address}
+})
